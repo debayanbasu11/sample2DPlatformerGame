beb9b5ac0ae7c53bcc241799aca6d1d2db81b901
Debayan Basu <debayanbasu11@gmail.com>
2020-10-23 19:15:38 +0530

    committing initial project files

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    [SerializeField] AudioClip CoinPickUpSFX;
+    [SerializeField] int pointsForCoinPickup = 50;
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+        AudioSource.PlayClipAtPoint(CoinPickUpSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+     // Config
+    [SerializeField] float LevelLoadDelay = 1f;
+    [SerializeField] float LevelExitSlowMoFactor = 0.1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel(){
+        Time.timeScale = LevelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+
+}
